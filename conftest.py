95e49d2fe569acc4d877e202f075aa2e1e0b185d
dragomir-yordanov <dragomirjordanov@gmail.com>
2025-09-24 16:44:51 +0300

    Update workflow and conftest for headless Chrome

--- conftest.py
+++ conftest.py
@@ -1,12 +1,18 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture
 def driver():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless=new")  # стартира Chrome без GUI
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
     service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
+    driver = webdriver.Chrome(service=service, options=chrome_options)
     driver.maximize_window()
     yield driver
     driver.quit()
